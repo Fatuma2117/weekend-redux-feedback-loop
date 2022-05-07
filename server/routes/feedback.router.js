c586551eca768b2ea5496f4e17453c49efae426d
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-05-07 16:12:04 -0500

    created post routes

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -15,6 +15,33 @@ router.get('/', (req, res) => {
 })
 
 
+router.post('/', (req, res) => {
+    const feedback = req.body;
+    console.log(feedback)
+    const sqlText = `
+      INSERT INTO "feedback"
+        ("feeling", "understanding", "support", "comments")
+          VALUES
+          ($1, $2,$3,$4)
+      `;
+    const sqlValues = [
+      feedback.feeling,
+      feedback.understanding,
+      feedback.support,
+      feedback.comment
+      
+    ]
+    pool.query(sqlText, sqlValues)
+      .then((dbResult) => {
+        console.log('Added feedback to the database', feedback);
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.log('Error making database query', error);
+        res.sendStatus(500); // Good server always responds
+      })
+  })
+
 
 
 
--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -1,48 +1,63 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector} from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { HashRouter as Link } from 'react-router-dom';
 import axios from 'axios';
+import { useState} from 'react';
 
 
 
 
-function ReviewForm() {
+function ReviewForm({getFeedback}) {
 
-    const dispatch = useDispatch();
-    const feedbackInfo = useSelector(store => store.feedbackReducer);
+    
+
+    const feedbackFeeling = useSelector(store => store.feelingReducer);
+    const feedbackUnderstanding = useSelector(store => store.understandingReducer);
+    const feedbackSupport = useSelector(store => store.supportReducer);
+    const feedbackComment = useSelector(store => store.commentReducer);
+  
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+
+        let addReview={
+           feeling: feedbackFeeling,
+            understanding: feedbackUnderstanding,
+           support: feedbackSupport,
+            comment: feedbackComment
+        }
+        console.log('Added new review to database',{ addReview})
+
         axios({
         method: 'POST',
         url: '/feedback',
-        data: {feeling}
+        data: addReview
     }).then((response) => {
-        console.log('this is the response of the post route', response)
-        
-        getFeedback();
+        console.log('response /post route', response)
+        alert('Thank you for the feedback!')
     }).catch((err) => {
         console.log('oops', err)
     })
 
+    }
     return(
-        <table>
-            <thead>
-                <tr>
-                    <th>Review Your Feedback</th>
-                </tr>
-            </thead>
-            <tbody>
-                {feedbackInfo.map((feedback) => {
-                    console.log(feedback);
-                    return(
-                    <tr key={feedback.id}>
-                        <td>{feedback.feeling}</td>
-                        <td>{feedback.understanding}</td>
-                        <td>{feedback.support}</td>
-                        <td>{feedback.comments}</td>
-                    </tr>)
-                })}
-            </tbody>
-        </table>
+    <>
+        <div>
+        <h1>Review Feedback</h1>
+            <h3>Feelings:{feedbackFeeling}</h3>
+            <h3>Understanding:{feedbackUnderstanding}</h3>
+            <h3>Support:{feedbackSupport}</h3>
+            <h3>Comments:{feedbackComment}</h3>
+        </div>
+        <form onSubmit={handleSubmit}>
+            <Link to="/ThanksForm">
+            <button>NEXT</button>
+          </Link>
+
+
+        </form>
+    
+    </>
     );
 
 
